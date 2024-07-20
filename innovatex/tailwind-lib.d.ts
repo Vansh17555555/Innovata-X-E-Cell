13e580c56be7ab787b74d8290dfa0a1721ad43d7
Vansh17555555 <vvansh739@gmail.com>
2024-07-20 17:16:51 +0530

    pushed the frontend

---
+++ innovatex/tailwind-lib.d.ts
@@ -0,0 +1,5 @@
+declare module 'tailwindcss/lib/util/flattenColorPalette' {
+    function flattenColorPalette(colors: Record<string, any>): Record<string, string>;
+    export default flattenColorPalette;
+  }
+  
